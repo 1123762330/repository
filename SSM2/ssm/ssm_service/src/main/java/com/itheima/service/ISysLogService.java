1a6698d9343e11087769ad98250ebc6eaddb0232
zly <1123762330@qq.com>
2018-12-23 11:52:05 +0800

    ssm项目五天完整版

---
+++ SSM2/ssm/ssm_service/src/main/java/com/itheima/service/ISysLogService.java
@@ -0,0 +1,14 @@
+package com.itheima.service;
+
+
+import com.itheima.SysLog;
+
+import java.util.List;
+
+public interface ISysLogService {
+    //添加日志
+    public void save(SysLog sysLog);
+
+    //查看日志
+    List<SysLog> findAll(int page, int size);
+}
---
+++ SSM2/ssm/ssm_service/src/main/java/com/itheima/service/impl/RoleServiceImpl.java
@@ -0,0 +1,50 @@
+package com.itheima.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.itheima.Permission;
+import com.itheima.Role;
+import com.itheima.dao.IRoleDao;
+import com.itheima.dao.IUserDao;
+import com.itheima.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("roleService")
+@Transactional
+public class RoleServiceImpl implements IRoleService {
+    @Autowired
+    private IRoleDao roleDao;
+    @Override
+    public List<Role> findAll(int page, int size) {
+        //调用分页插件,实现分页功能
+        PageHelper.startPage(page,size);
+        return roleDao.findAll();
+    }
+
+    @Override
+    public void save(Role role) {
+        roleDao.save(role);
+    }
+
+    @Override
+    public Role findById(String roleId) {
+        return roleDao.findById(roleId);
+    }
+
+    //根据roleid查询可以添加的权限
+    @Override
+    public List<Permission> findOtherRoles(String roleId) {
+        return roleDao.findOtherRoles(roleId);
+    }
+
+    //给角色添加权限
+    @Override
+    public void addPermissionToRole(String roleId, String[] permissionIds) {
+        for (String permissionId : permissionIds) {
+            roleDao.addPermissionToRole(roleId,permissionId);
+        }
+    }
+}
